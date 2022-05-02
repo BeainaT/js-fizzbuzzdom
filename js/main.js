7952a61afd07896b3e00ea811ab0b10b5e3950c6
BeainaT <tania.df@hotmail.it>
2022-05-02 16:43:12 +0200

    added second milestone

--- js/main.js
+++ js/main.js
@@ -18,6 +18,21 @@ for (let i = 1; i <= 100; i++) {
 }
 //MILESTONE 2
 //append html element with the consistent number or string in DOM existing container
-
+const list = document.querySelector(".list");
+for (let i = 1; i <= 100; i++) {
+    const liElement = document.createElement("li");
+    let fizzNumber = i % 3 === 0;
+    let buzzNumber = i % 5 === 0;
+    if ((fizzNumber) && (buzzNumber)) {
+        liElement.innerText = "fizzbuzz";
+    } else if (fizzNumber) {
+        liElement.innerText = "fizz";
+    } else if (buzzNumber) {
+        liElement.innerText = "buzz";
+    } else {
+        liElement.innerText = i;
+    }
+    list.append(liElement);
+}
 //MILESTONE 3
 //apply different style for different outcome
