83915dbfe9f8a2c92e202600e15fb3f7fdba5ccf
BeainaT <tania.df@hotmail.it>
2022-05-02 15:19:41 +0200

    added basis file + first js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//Print number from 1 to 100 on console
+for (let i = 1; i <= 100; i++) {
+    console.log(i);
+}
+//MILESTONE 1
+//Print "Fizz" for multiples of 3
+//Print "Buzz" for multiples of 5
+//Print "FizzBuzz" for multiples of 3 and 5
+
+//MILESTONE 2
+//append html element with the consistent number or string in DOM existing container
+
+//MILESTONE 3
+//apply different style for different outcome
